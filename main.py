80450b284cb41dc72a65860e4d70b9a2f918aebf
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-02-01 13:00:25 +0300

    Treasure Island game final commit

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 print("Welcome to the Treasure Island. \nYour mission is to find the treasure.")
 
-direction = input("Left or Right?").lower()
+direction = input('You\'re at a crossroads, which way do you go? Type "Left" or "Right"?\n').lower()
 
 if direction == "right":
     print("Game over, should have gone left!")
 
 elif direction == "left":
-    island = input("Swim or wait?").lower()
+    island = input("You've come to a lake. There is an Island in the middle of the lake. "
+                   "Type 'wait' to wait for boat or 'swim' to swim across.\n").lower()
 
     if island == "swim":
-        print("Game over! You got eaten!")
+        print("Game over! You got eaten by a trout!")
 
     elif island == "wait":
-        door = input("Which door! Red, blue or yellow?").lower()
+        door = input("You arrive at the Island unharmed. There is a house with three doors"
+                     " red, blue and yellow. Which door will you enter?\n").lower()
 
         if door == "yellow":
             print("You win!")
